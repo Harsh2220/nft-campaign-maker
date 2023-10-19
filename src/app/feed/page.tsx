651736706f00d2e288c18e489177969c850166c6
Harsh <harshgajjar145@gmail.com>
2023-10-19 23:34:32 +0530

    fixes

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -14,15 +14,17 @@ export default function Feed() {
 
   return (
     <div className="container mx-auto flex items-center gap-10 my-10 flex-wrap p-10">
-      {data ?
+      {data instanceof Array ? (
         <>
           {data?.map((address: string) => (
             <Card key={address} address={address} />
           ))}
         </>
-        : <div className="flex justify-center items-center w-full h-full">
+      ) : (
+        <div className="flex justify-center items-center w-full h-full">
           <h1 className="font-bold text-2xl">No campaigns</h1>
-        </div>}
+        </div>
+      )}
     </div>
-  )
+  );
 }
